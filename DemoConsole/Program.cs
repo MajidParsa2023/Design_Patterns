706ad7fdea875fa5913cfeea6a768edaedc036f7
MajidEbadiparsa <MajidEbadiparsaJob@gmail.com>
2025-02-16 11:01:16 +0100

    Init files

---
+++ DemoConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatternsDemo.Singleton_Pattern;
+
+Console.WriteLine("Start Demo ...!!!");
+
+Console.WriteLine();
+Console.WriteLine("*** SINGLETON ***");
+Singleton_NewMethod.Instance.DoSomething();
+Singleton_OldMethod.Instance.DoSomething();
+
+Console.WriteLine();
+Console.WriteLine("*** SINGLETON ***");
+Singleton_NewMethod.Instance.DoSomething();
+Singleton_OldMethod.Instance.DoSomething();
+
+Console.WriteLine("End Demo ...!!!");
