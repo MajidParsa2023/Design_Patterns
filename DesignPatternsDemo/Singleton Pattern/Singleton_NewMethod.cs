430f9c9b0856f4fc34d053f9489dd21033fe3ede
MajidEbadiparsa <MajidEbadiparsaJob@gmail.com>
2025-02-16 11:20:19 +0100

    Init Projects

---
+++ DesignPatternsDemo/Singleton Pattern/Singleton_NewMethod.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatternsDemo.Singleton_Pattern
+{
+	public class Singleton_NewMethod
+	{
+		private static readonly Lazy<Singleton_NewMethod> _instance = new(() => new Singleton_NewMethod());
+
+		private Singleton_NewMethod()
+		{
+		}
+
+		public static Singleton_NewMethod Instance => _instance.Value;
+
+		public void DoSomething() => Console.WriteLine("Object is doing something by new method...");
+	}
+}
---
+++ DesignPatternsDemo/Singleton Pattern/Singleton_OldMethod.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPatternsDemo.Singleton_Pattern
+{
+	public class Singleton_OldMethod
+	{
+		private static Singleton_OldMethod? _instance;
+		private static readonly object _lock = new();
+
+		private Singleton_OldMethod()
+		{
+		}
+
+		public static Singleton_OldMethod Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (_lock)
+					{
+						_instance ??= new Singleton_OldMethod();
+
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+		public void DoSomething() => Console.WriteLine("Object is doing something by old method...");
+	}
+}
